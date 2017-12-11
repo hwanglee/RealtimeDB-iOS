f69a93d9a36f24cc83717c81633a4d469212faf1
hwanglee <hwangseung@gmail.com>
2017-12-11 04:55:34 -0500

    added profile page

--- RealtimeDB/Controller/ProfileController.swift
+++ RealtimeDB/Controller/ProfileController.swift
@@ -10,31 +10,116 @@ import UIKit
 import Firebase
 
 class ProfileController: UIViewController {
-
+    
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var firstNameLabel: UILabel!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet var lastNameLabel: UILabel!
+    @IBOutlet var addressTextField: UITextField!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var phoneTextField: UITextField!
+    @IBOutlet var phoneLabel: UILabel!
+    var db : DatabaseReference!
+    var user : User?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        db = Database.database().reference()
         
         setupNavbar()
-        // Do any additional setup after loading the view.
+        setupLabels()
     }
-
+    
     func setupNavbar() {
         self.title = "Profile"
+        editHidden(label: false, textField: true)
         
         let locationButton = UIButton(type: .roundedRect)
         locationButton.setTitle("Logout", for: .normal)
         locationButton.addTarget(self, action: #selector(logout), for: .touchUpInside)
         let barButton = UIBarButtonItem(customView: locationButton)
         self.navigationItem.leftBarButtonItem = barButton
+        
+        addEditButton()
     }
-
+    
+    @objc func edit() {
+        editHidden(label: true, textField: false)
+        
+        self.view.endEditing(true)
+        let editButton = UIButton(type: .roundedRect)
+        editButton.setTitle("Done", for: .normal)
+        editButton.addTarget(self, action: #selector(done), for: .touchUpInside)
+        let barButton2 = UIBarButtonItem(customView: editButton)
+        self.navigationItem.rightBarButtonItem = barButton2
+    }
+    
+    @objc func done() {
+        editHidden(label: false, textField: true)
+        
+        let uid = Auth.auth().currentUser?.uid
+        var data : [String : AnyObject] = [:]
+        
+        if let user = user {
+            data = ["address": addressTextField.text, "cellPhone": phoneTextField.text, "createdAt": user.createdAt!, "email": user.email!,  "firstName": firstNameTextField.text, "id": user.id!, "lastName": lastNameTextField.text, "subscription": user.subscription!, "type": user.type!] as [String : AnyObject]
+        }
+        
+        db.child("users").child(uid!).setValue(data) { (error, ref) in
+            if error != nil {
+                print("error")
+            } else {
+                self.addEditButton()
+                self.view.endEditing(true)
+            }
+        }
+        
+    }
+    
+    func editHidden(label: Bool, textField: Bool) {
+        firstNameLabel.isHidden = label
+        lastNameLabel.isHidden = label
+        addressLabel.isHidden = label
+        phoneLabel.isHidden = label
+        firstNameTextField.isHidden = textField
+        lastNameTextField.isHidden = textField
+        addressTextField.isHidden = textField
+        phoneTextField.isHidden = textField
+    }
+    
+    func addEditButton() {
+        let editButton = UIButton(type: .roundedRect)
+        editButton.setTitle("Edit", for: .normal)
+        editButton.addTarget(self, action: #selector(edit), for: .touchUpInside)
+        let barButton2 = UIBarButtonItem(customView: editButton)
+        self.navigationItem.rightBarButtonItem = barButton2
+    }
+    
     @objc func logout() {
         try! Auth.auth().signOut()
         
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginController")
-        //                    self.navigationController!.pushViewController(vc!, animated: true)
         
         let navController = UINavigationController(rootViewController: vc!)
         self.present(navController, animated: true, completion: nil)
+    }
+    
+    func setupLabels() {
+        let uid = Auth.auth().currentUser?.uid
+        db.child("users").child(uid!).observe(.value) { (snapshot) in
+            self.user = User(snapshot: snapshot)
+            
+            if let user = self.user {
+                self.firstNameLabel.text = user.firstName!
+                self.firstNameTextField.text = user.firstName!
+                self.lastNameLabel.text = user.lastName!
+                self.lastNameTextField.text = user.lastName!
+                self.addressLabel.text = user.address!
+                self.addressTextField.text = user.address!
+                self.phoneLabel.text = user.cellPhone!
+                self.phoneTextField.text = user.cellPhone!
+            }
+        }
+        
+        
     }
 }
---
+++ RealtimeDB/User.swift
@@ -0,0 +1,39 @@
+//
+//  User.swift
+//  RealtimeDB
+//
+//  Created by Hwang Lee on 12/11/17.
+//  Copyright © 2017 Hwang Lee. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class User : NSObject {
+    var address: String?
+    var cellPhone: String?
+    var createdAt: Int?
+    var email: String?
+    var firstName: String?
+    var id: String?
+    var lastName: String?
+    var subscription: Bool?
+    var type: String?
+    var db = Database.database().reference()
+    
+    init(snapshot: DataSnapshot) {
+        
+        let values = snapshot.value as! [String: AnyObject]
+        print(values)
+        self.address = values["address"] as? String
+        self.cellPhone = values["cellPhone"] as? String
+        self.createdAt = values["createdAt"] as? Int
+        self.email = values["email"] as? String
+        self.firstName = values["firstName"] as? String
+        self.id = values["id"] as? String
+        self.lastName = values["lastName"] as? String
+        self.subscription = values["subscription"] as? Bool
+        self.type = values["type"] as? String
+        
+    }
+}
